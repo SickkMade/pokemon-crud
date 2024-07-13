8afb65ee4aedda65e00d4f1fec71e60da5cea0b2
Jack Meyer <jaakmyr@gmail.com>
2024-07-12 23:15:04 -0700

    added user's and adding pokemon, saving to user in db, displaying username, pokemon, and pokemon pulled from api

--- controllers/pokemon-builder.js
+++ controllers/pokemon-builder.js
@@ -1,11 +1,30 @@
 const controller = require("../models/pokemon-builder.js")
+const user = controller.AddToUser
 
 module.exports = {
     getPokemonBuilder: async (req, res) => {
         const pokeList = await controller.getPokeList()
-        res.render("../views/pokemon-builder.ejs", {pokemon: pokeList})
+        let nameQuery = req.query.name;
+
+        if(nameQuery === undefined){
+            res.redirect('/')
+            return
+        } else {
+            nameQuery = nameQuery.toUpperCase()
+        }
+        const doesTrainerExist = await user.exists({name: nameQuery})
+        if(!doesTrainerExist){
+            await user.create({pokemon: [], name:nameQuery})
+        }
+        const trainer = await user.findOne({name:nameQuery})
+        res.render("../views/pokemon-builder.ejs", {pokemon: pokeList, name: nameQuery, chosenPokemon: trainer.pokemon})
     },
-    addToUserTeam: (req, res) => {
-        res.redirect('/pokemon-builder')
+    addToUserTeam: async (req, res) => {
+        const trainer = await user.findOne({name:req.query.name.toUpperCase()})
+        if(req.body['pokemon-151'] != ""){
+            trainer.pokemon.push(req.body['pokemon-151'])
+            trainer.save()
+        }
+        res.redirect('/pokemon-builder?name='+req.query.name)
     },
 }
